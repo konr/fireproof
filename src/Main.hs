508b4381aa7ac1ecffab59f1eb599f58a55bec45
Konrad Scorciapino <scorciapino@gmail.com>
2018-03-28 18:36:41 -0300

    add command line args

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,40 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
 import           Control.Monad.IO.Class
-import           Logic
-import           Model
+import           System.Console.CmdArgs
+
 import           Adapter
+import           Logic
+import qualified Model                  as Model
+
+
+data CLIArgs = CLIArgs {netWorth   :: Float
+                       , spend     :: Float
+                       , years     :: Int
+                       , interest  :: Float
+                       , startYear :: Int
+                       , inflation :: Float}
+             deriving (Show, Data, Typeable)
+
+defaults = CLIArgs{netWorth = def &= help "Your current net worth"
+                , years = 10 &= help "Simulation years"
+                , spend = def &= help "Current yearly spend"
+                , interest = def &= help "Yearly interest rate"
+                , startYear = def &= help "Start year"
+                , inflation = def &= help "Yearly inflation"}
+
+cliToFireParams  :: CLIArgs -> Model.FireParams
+cliToFireParams cli =
+  Model.FireParams (Model.Year (startYear cli) (inflation cli) (interest cli))
+                   (Model.Portfolio (netWorth cli) (spend cli))
 
 main :: IO ()
 main = do
   putStrLn $ "Will you be able to FIRE?"
-  mapM_ putStrLn (parameters firão)
+  cliArgs <- cmdArgs defaults
+  let fire = cliToFireParams cliArgs
+  mapM_ putStrLn (parameters fire)
   putStrLn ""
-  putStrLn $ "Projections:" 
-  mapM_ (putStrLn . yearNetWorth) $ projections
+  putStrLn $ "Projections:"
+  mapM_ (putStrLn . yearNetWorth) $ take (years cliArgs) $ iterate updateFireParams fire
