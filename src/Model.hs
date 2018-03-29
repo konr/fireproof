e5b2da4eee1f35a89fa8d126c8e84cfb87b65fed
Konrad Scorciapino <scorciapino@gmail.com>
2018-03-29 07:10:40 -0300

    add logic tests

--- src/Model.hs
+++ src/Model.hs
@@ -4,13 +4,13 @@ data Portfolio =
   Portfolio {
     netWorth     :: Float -- FIXME using newtypes?
   , yearlySpend  :: Float
-  } deriving (Show)
+  } deriving (Show, Eq)
 
 data Year =
   Year {
     yearNumber :: Int
   , inflation :: Float
   , interestRate :: Float
-  } deriving (Show)
+  } deriving (Show, Eq)
 
-data FireParams = FireParams Year Portfolio deriving (Show)
+data FireParams = FireParams Year Portfolio deriving (Show, Eq)
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,12 +2,14 @@ import           Test.Hspec
 
 import qualified Adapter
 import qualified Logic
-import           Model
+import qualified Model
 
-sampleParams = Model.FireParams (Model.Year 2018 0.03 0.08) (Model.Portfolio 1000.0 60.0)
+sampleYear = Model.Year 2018 0.03 0.08
+samplePortfolio = Model.Portfolio 1000.0 60.0
+sampleParams = Model.FireParams sampleYear samplePortfolio
 
-main :: IO ()
-main = hspec $ do
+adapterTests :: IO ()
+adapterTests = hspec $ do
   describe "yearNetWorth adapter" $ do
     it "should output the year and the net worth" $ do
       (Adapter.yearNetWorth sampleParams) `shouldBe` "[2018] Net Worth: $1000.0"
@@ -18,3 +20,22 @@ main = hspec $ do
                                                    ," - Interest rate: 8.0e-2"
                                                    ," - Start Net Worth: 1000.0"
                                                    ," - Yearly Spend: 60.0"]
+
+logicTests :: IO ()
+logicTests = hspec $ do
+  describe "updateAmount" $ do
+    it "should update portfolio amount, given interest and inflation" $ do
+      (Logic.updateAmount sampleYear samplePortfolio) `shouldBe` (Model.Portfolio 987.00006 60.0)
+  describe "updateYear" $ do
+    it "should increment year" $ do
+      (Logic.updateYear sampleYear) `shouldBe` (Model.Year 2019 0.03 0.08)
+  describe "updateFireParams" $ do
+    it "should increment year and portfolio amount" $ do
+      (Logic.updateFireParams sampleParams) `shouldBe`
+        (Model.FireParams (Model.Year 2019 0.03 0.08) (Model.Portfolio 987.00006 60.0))
+
+
+main :: IO ()
+main = do
+  adapterTests
+  logicTests
